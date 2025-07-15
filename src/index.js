6fcba9f2eaa59d39e9de53f5e9bd6f222c3a0523
Christian R. Picone <ch.picone@gmail.com>
2025-07-15 20:21:53 +0200

    update

--- src/index.js
+++ src/index.js
@@ -30,20 +30,24 @@ module.exports = function createJwtMiddleware({
         }
         return false;
     })) {
+        console.log("Path is public => next()")
         return next();
     }
 
+    console.log("JwtMiddleware: step 1")
     const authHeader = req.headers['authorization'];
     let token = authHeader && authHeader.split(' ')[1];
     if(!token && useCookie && req.cookies) token = req.cookies.token;
 
+    console.log("JwtMiddleware: step 2")
     if (!token) {
       if(redirectTo)
-        res.redirect(redirectTo);
+        return res.redirect(redirectTo);
       else  
         return res.status(401).json({ error: 'Missing token' });
     }
 
+    console.log("JwtMiddleware: step 3")
     jwt.verify(token, secret, (err, decoded) => {
       if (err) {
         if(redirectTo)
@@ -53,16 +57,17 @@ module.exports = function createJwtMiddleware({
       }
       req.user = decoded;
 
+      console.log("JwtMiddleware: step 4")
       if(useCookie){
         newToken = createToken({ user: decoded })
-        console.log(newToken)
+        console.log("JwtMiddleware: " + newToken)
         res.cookie('token', newToken, {
           httpOnly: true,
           secure: false,
           sameSite: 'lax'
         })
       }
-
+      console.log("JwtMiddleware: step 5")
       next();
 
     });
